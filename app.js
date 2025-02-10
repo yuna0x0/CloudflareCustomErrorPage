b23e89c6763d92bbbf961f73fcdb4aed23231e53
yuna0x0 <yuna@yuna0x0.com>
2025-02-10 17:54:18 +0800

    Support dev watch

--- app.js
+++ app.js
@@ -1,20 +1,53 @@
 const ejs = require("ejs");
-const configs = [require("./config.js")/* , require("./i18n/config-zh-CN.js") */];
 const fs = require("fs");
-configs.forEach((config) =>
-  config.builderConfig.map((item) => {
-    let html = ejs.render(
-      fs.readFileSync("./ejs/index.ejs", { encoding: "utf8" }),
-      {
-        config: item,
-        i18n: config.i18n,
-        helper: {},
-      },
-      {
-        root: "./ejs/index.ejs",
-        filename: "./ejs/index.ejs",
-      }
+const chokidar = require('chokidar');
+const express = require('express');
+
+const requireUncached = (module) => {
+    delete require.cache[require.resolve(module)];
+    return require(module);
+};
+
+const buildPages = () => {
+    const configs = [requireUncached("./config.js")/* , requireUncached("./i18n/config-zh-CN.js") */];
+    configs.forEach((config) =>
+        config.builderConfig.map((item) => {
+            let html = ejs.render(
+                fs.readFileSync("./ejs/index.ejs", { encoding: "utf8" }),
+                {
+                    config: item,
+                    i18n: config.i18n,
+                    helper: {},
+                },
+                {
+                    root: "./ejs/index.ejs",
+                    filename: "./ejs/index.ejs",
+                }
+            );
+            fs.writeFileSync(`./public/${item.fileName}`, html, { encoding: "utf8" });
+        })
     );
-    fs.writeFileSync(`./public/${item.fileName}`, html, { encoding: "utf8" });
-  })
-);
+};
+
+const startWebServer = () => {
+    express()
+        .use(express.static('public'))
+        .listen(3000);
+    console.log("Listening on http://localhost:3000");
+};
+
+if (process.argv.length == 3 && process.argv[2] == "--dev") {
+    startWebServer();
+    chokidar.watch(['./config.js', './ejs/']).on('all', (event, path) => {
+        console.log(event, path);
+        buildPages();
+        console.log("Rebuilt pages\n");
+    });
+} else if (process.argv.length == 3 && process.argv[2] == "--build") {
+    buildPages();
+} else if (process.argv.length == 2) {
+    startWebServer();
+} else {
+    console.log("Usage: node app.js [--dev|--build]");
+    process.exit(1);
+}
